007af90a4aa75be6108ce982b4cd7028ee6c6e53
Awsome-Areej <41355424+Awsome-Areej@users.noreply.github.com>
2018-09-01 14:33:26 -0700

    Create Madlibs.py

---
+++ Madlibs.py
@@ -0,0 +1,54 @@
+def instructions():
+    print "I will ask you for words and with those words we will make a story."  
+    
+def madlibs():
+    print "Welcome to madlibs the worlds most famous word game."
+    doyou = input ("Do you know how to play?")
+    if doyou == "yes":
+        adjective1 = input ("Give me an adjective.")
+        print "Very good."
+        noun1 = input ("A noun")
+        print "Good job"
+        animal1 = input ("Your favourite animal")
+        print "Nice!"
+        food1 = input ("A type of food")
+        print "Yum."
+        food2 = input ("Another food")
+        print "Three more to go"
+        adjective2 = input ("An adjective")
+        print "Nice"
+        noun2 = input ("A noun")
+        print "Last one"
+        animal2 = input ("An animal")
+        doyou2 = "Well done,would you like to hear the story?"
+        if doyou2 == "yes":
+            print "Topic "  +  animal1
+            print "The animal " +  animal1 +  " are surprisingly "  +  adjective1 + " and  "  + adjective2 + ". " + " They love to eat " + food1 + " and " + food2 + " and rarely eat " + animal2 + ". " + " They either live in the " + noun1 + " or the "  +  noun2 + "."
+            
+    else:
+        print "Here are the instructions"
+        instructions()
+        print "Welcome to madlibs the worlds most famous word game."
+    doyou = input ("Do you know how to play?")
+    if doyou == "yes":
+        adjective1 = input ("Give me an adjective.")
+        print "Very good."
+        noun1 = input ("A noun")
+        print "Good job"
+        animal1 = input ("Your favourite animal")
+        print "Nice!"
+        food1 = input ("A type of food")
+        print "Yum."
+        food2 = input ("Another food")
+        print "Three more to go"
+        adjective2 = input ("An adjective")
+        print "Nice"
+        noun2 = input ("A noun")
+        print "Last one"
+        animal2 = input ("An animal")
+        doyou2 = "Well done,would you like to hear the story?"
+        if doyou2 == "yes":
+            print "Topic "  +  animal1
+            print "The animal " +  animal1 +  " are surprisingly "  +  adjective1 + " and  "  + adjective2 + ". " + " They love to eat " + food1 + " and " + food2 + " and rarely eat " + animal2 + ". " + " They either live in the " + noun1 + " or the "  +  noun2 + "."
+
+        
